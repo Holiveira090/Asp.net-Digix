a9b9b787c52db987892eaa5186c9cfc9d222409c
Holiveira090 <h.oliveira2005@hotmail.com>
2025-05-12 16:53:55 -0400

    Começo de react

--- Aula_1/obj/Debug/net8.0/Aula_1.AssemblyInfo.cs
+++ Aula_1/obj/Debug/net8.0/Aula_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f6b40b76c7e3fa11050180aec0d296e66be160b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0295313c5a8aa472554a17fedc5ac111960bcf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SistemaEscolarAPI/obj/Debug/net8.0/SistemaEscolarAPI.AssemblyInfo.cs
+++ SistemaEscolarAPI/obj/Debug/net8.0/SistemaEscolarAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEscolarAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f6b40b76c7e3fa11050180aec0d296e66be160b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0295313c5a8aa472554a17fedc5ac111960bcf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaEscolarAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaEscolarAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
